6d124b66d9bc41606ac1417745ecec9cc2281bc5
pumanitro <pumanitro@gmail.com>
2017-04-15 18:05:01 +0200

    Tried types and enum.

--- Tests/script.ts
+++ Tests/script.ts
@@ -2,3 +2,24 @@
  * Created by puman on 15.04.2017.
  */
 
+let myName = 'Patryk';
+//myName = 28;
+
+let myAge = 22;
+myAge = 24.5;
+
+let myRealName: number;
+//myRealName = "blue";
+
+let hobbies = ["Cooking",25];
+hobbies = ["blue"];
+console.log(hobbies);
+
+enum Color{
+    Gray, //0
+    Green = 100, //100
+    Blue //2
+}
+
+let myColor: Color = Color.Gray;
+console.log(myColor);
