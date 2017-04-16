0585aaee96068ad2d07a3cd37438d979b9a2ff13
pumanitro <pumanitro@gmail.com>
2017-04-17 00:56:48 +0200

    Exercise 3 - changing code into TS. Using classes getters/setters, inheritance and more.

--- Class exercise/script.ts
+++ Class exercise/script.ts
@@ -1,55 +1,65 @@
 
 // Exercise 1 - How was your TypeScript Class?
-function Car(name) {
-    this.name = name;
-    this.acceleration = 0;
+class Car{
+    name: string;
+    acceleration : number = 0;
 
-    this.honk = function() {
+    constructor(name: string){
+        this.name = name;
+    }
+
+    honk(): void {
         console.log("Toooooooooot!");
-    };
+    }
 
-    this.accelerate = function(speed) {
-        this.acceleration = this.acceleration + speed;
+    accelerate(speed: number): void {
+        this.acceleration += speed;
     }
 }
-var car = new Car("BMW");
+
+let car = new Car("BMW");
 car.honk();
 console.log(car.acceleration);
 car.accelerate(10);
 console.log(car.acceleration);
 
 // Exercise 2 - Two objects, based on each other ...
-var baseObject = {
-    width: 0,
-    length: 0
-};
-var rectangle = Object.create(baseObject);
+class BaseObject {
+    width: number = 0;
+    length: number = 0;
+}
+
+class Rectangle extends BaseObject{
+    calcSize(): number {
+        return this.width * this.length;
+    }
+}
+let rectangle = new Rectangle();
+
 rectangle.width = 5;
-rectangle.length = 2;
-rectangle.calcSize = function() {
-    return this.width * this.length;
-};
+rectangle.length = 10;
 console.log(rectangle.calcSize());
 
 // Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
-var person = {
-    _firstName: ""
-};
-Object.defineProperty(person, "firstName", {
-    get: function () {
+
+class Person {
+    _firstName = "";
+
+    get firstName(){
         return this._firstName;
-    },
-    set: function (value) {
-        if (value.length > 3) {
+    }
+
+    set firstName(value){
+        if(value.length > 3)
             this._firstName = value;
-        }
-        else {
+        else
             this._firstName = "";
-        }
-    },
-    enumerable: true,
-    configurable: true
-});
+    }
+
+}
+
+let person = new Person();
+
 console.log(person.firstName);
 person.firstName = "Ma";
 console.log(person.firstName);
