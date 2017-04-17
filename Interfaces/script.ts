5286fb3f458fb61896a3e57a6e4fb85a7b28acfa
pumanitro <pumanitro@gmail.com>
2017-04-17 20:25:54 +0200

    Using interfaces with classes.

--- Interfaces/script.ts
+++ Interfaces/script.ts
@@ -6,17 +6,39 @@ interface  NamedPerson{
     firstName: string;
     age?: number;
     [propName: string]: any;
+
+    greet(lastName: string): void;
 }
 
 function greet(person: NamedPerson){
     console.log("Hello, " + person.firstName);
 }
 
-const person = {
-  firstName: "Max",
-    hobbies: ["Cooking","Sports"]
+const person: NamedPerson = {
+    firstName: "Max",
+    hobbies: ["Cooking","Sports"],
+    greet(lastName: string){
+      console.log(`Hi, I'm ${this.firstName} ${lastName}`);
+    }
 };
 
-greet({firstName: "Max", age: 27});
+//greet({firstName: "Max", age: 27});
 greet(person);
+person.greet("Janik");
+
+class Person implements NamedPerson{[propName: string]: any;
+    firstName: string;
+    //age: number;
+    lastName: string;
+
+    greet(lastName: string): void {
+        console.log(`Hi, I'm ${this.firstName} ${lastName}`);
+    }
+}
+
+const myPerson = new Person();
+myPerson.firstName = "Maximilian";
+myPerson.lastName = "Anything";
+greet(myPerson);
+myPerson.greet(myPerson.lastName);
 
