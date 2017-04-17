fd8468f73be715fc7996acd92d81e65ea99f7d73
pumanitro <pumanitro@gmail.com>
2017-04-17 16:19:09 +0200

    Loaded first modules.

--- Modules/math/circle.ts
+++ Modules/math/circle.ts
@@ -2,8 +2,8 @@
  * Created by puman on 17.04.2017.
  */
 
-const PI = 3.14;
+export const PI = 3.14;
 
-function calculateCircumference(diameter: number){
+export function calculateCircumference(diameter: number){
     return diameter * PI;
 }
--- Modules/script.ts
+++ Modules/script.ts
@@ -2,4 +2,8 @@
  * Created by puman on 17.04.2017.
  */
 
+import { PI, calculateCircumference} from "./math/circle";
 
+console.log(PI);
+console.log(calculateCircumference(10));
+//console.log();
