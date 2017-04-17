7137c1e356dbdc770e27854722f3eabbe6ffbfae
pumanitro <pumanitro@gmail.com>
2017-04-17 11:07:10 +0200

    Created my first namespace in TS

---
+++ Namespaces and modules/script.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by puman on 17.04.2017.
+ */
+
+namespace  MyMath {
+
+    const PI = 3.14;
+
+    export function calculateCircumference(diameter: number){
+        return diameter * PI;
+    }
+
+    export function  calculateRectangle(width: number, length: number){
+        return width * length;
+    }
+}
+
+console.log(MyMath.calculateRectangle(10,20));
+console.log(MyMath.calculateCircumference(3));
