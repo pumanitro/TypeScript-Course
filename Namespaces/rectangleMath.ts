928a165fc5cdcf92b7c489c0f0b86a9e4edb1d14
pumanitro <pumanitro@gmail.com>
2017-04-17 11:27:26 +0200

    Renamed folder.

---
+++ Namespaces/rectangleMath.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by puman on 17.04.2017.
+ */
+
+namespace  MyMath {
+
+    export namespace Circle{
+        const PI = 3.14;
+
+        export function calculateCircumference(diameter: number){
+            return diameter * PI;
+        }
+    }
+
+}
--- Namespaces/script.ts
+++ Namespaces/script.ts
@@ -5,5 +5,7 @@
 /// <reference path="circleMath.ts"/>
 /// <reference path="rectangleMath.ts"/>
 
+import CircleMath = MyMath.Circle;
+
 console.log(MyMath.calculateRectangle(10,20));
-console.log(MyMath.calculateCircumference(3));
+console.log(CircleMath.calculateCircumference(3));
